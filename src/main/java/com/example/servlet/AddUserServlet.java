035f1467115b958d56e52b95927aac8867e06e91
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-02-02 23:35:59 +0400

    4

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -18,7 +18,7 @@ public class AddUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  ServletException,IOException{
-        String path = "/add";
+        String path = "jsp/add.jsp";
         RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(path);
         try {
             requestDispatcher.forward(req, resp);
