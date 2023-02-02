b6a26b8e537159423f5511f35ea329fb1b197f2c
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-02-02 22:45:11 +0400

    1

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,34 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
     //write your code here!
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        String f_name = req.getParameter("firstName");
+        String l_name = req.getParameter("lastName");
+
+        User user = new User(f_name, l_name);
+        Warehouse.getInstance().addUser(user);
+
+        req.setAttribute("user", user);
+
+        resp.sendRedirect("/add");
+
+
+    }
+
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,24 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Set<User> users = Warehouse.getInstance().getUsers();
+        req.setAttribute("users", users);
+
+        resp.sendRedirect("/users");
+    }
     //write your code here!
 }
